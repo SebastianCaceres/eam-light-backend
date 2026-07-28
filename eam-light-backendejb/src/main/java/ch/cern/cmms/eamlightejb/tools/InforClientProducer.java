9ba7bcd2213f16bd04ae4da09012bec8fd02ed34
SebastianCaceres <sebastiancaceres071@gmail.com>
2026-07-28 15:24:56 -0400

    Wire Spring Data JPA Repositories in InforClientProducer and update eam-wshub-core submodule

--- eam-light-backendejb/src/main/java/ch/cern/cmms/eamlightejb/tools/InforClientProducer.java
+++ eam-light-backendejb/src/main/java/ch/cern/cmms/eamlightejb/tools/InforClientProducer.java
@@ -5,6 +5,7 @@ import ch.cern.cmms.eamlightejb.data.ApplicationData;
 import ch.cern.cmms.eamlightejb.tools.soaphandler.SOAPHandlerResolver;
 import ch.cern.eam.wshub.core.client.InforClient;
 import ch.cern.eam.wshub.core.interceptors.InforInterceptor;
+import ch.cern.eam.wshub.core.repositories.*;
 import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.transport.http.HTTPConduit;
 import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
@@ -34,6 +35,51 @@ public class InforClientProducer {
     @Autowired(required = false)
     private InforInterceptor inforInterceptor;
 
+    @Autowired(required = false)
+    private WorkOrderRepository workOrderRepository;
+
+    @Autowired(required = false)
+    private ActivityRepository activityRepository;
+
+    @Autowired(required = false)
+    private LaborBookingRepository laborBookingRepository;
+
+    @Autowired(required = false)
+    private FindingRepository findingRepository;
+
+    @Autowired(required = false)
+    private PartRepository partRepository;
+
+    @Autowired(required = false)
+    private EquipmentRepository equipmentRepository;
+
+    @Autowired(required = false)
+    private EmployeeRepository employeeRepository;
+
+    @Autowired(required = false)
+    private CategoryRepository categoryRepository;
+
+    @Autowired(required = false)
+    private InforDocumentRepository inforDocumentRepository;
+
+    @Autowired(required = false)
+    private InforDocEntityRepository inforDocEntityRepository;
+
+    @Autowired(required = false)
+    private InforCaseRepository inforCaseRepository;
+
+    @Autowired(required = false)
+    private InforCaseTaskRepository inforCaseTaskRepository;
+
+    @Autowired(required = false)
+    private EquipmentPMScheduleRepository equipmentPMScheduleRepository;
+
+    @Autowired(required = false)
+    private EquipmentWarrantyRepository equipmentWarrantyRepository;
+
+    @Autowired(required = false)
+    private EAMUserRepository eamUserRepository;
+
     @Bean
     public InforClient inforClient() {
         try {
@@ -47,6 +93,21 @@ public class InforClientProducer {
                     .withInforInterceptor(inforInterceptor)
                     .withLogger(Logger.getLogger("wshublogger"))
                     .withCache(externalCache.getCacheMap())
+                    .withWorkOrderRepository(workOrderRepository)
+                    .withActivityRepository(activityRepository)
+                    .withLaborBookingRepository(laborBookingRepository)
+                    .withFindingRepository(findingRepository)
+                    .withPartRepository(partRepository)
+                    .withEquipmentRepository(equipmentRepository)
+                    .withEmployeeRepository(employeeRepository)
+                    .withCategoryRepository(categoryRepository)
+                    .withInforDocumentRepository(inforDocumentRepository)
+                    .withInforDocEntityRepository(inforDocEntityRepository)
+                    .withInforCaseRepository(inforCaseRepository)
+                    .withInforCaseTaskRepository(inforCaseTaskRepository)
+                    .withEquipmentPMScheduleRepository(equipmentPMScheduleRepository)
+                    .withEquipmentWarrantyRepository(equipmentWarrantyRepository)
+                    .withEAMUserRepository(eamUserRepository)
                     .localizeResults(false)
                     .build();
 
